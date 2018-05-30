1f39f4ec3464effba58d60a2293a2a6d40c120c6
aereneth <zerify@gmail.com>
2018-05-30 23:02:58 +0800

    Inventory Table

--- application/views/inventory/index.php
+++ application/views/inventory/index.php
@@ -4,7 +4,16 @@
     <div class="card-panel">
         <table class="striped responsive-table highlight">
             <thead>
-                
+                <tr>
+					<th>Book ID</th>
+					<th>ISBN</th>
+					<th>Title</th>
+					<th>Author</th>
+					<th>Category</th>
+					<th>Publisher</th>
+					<th>Copies</th>
+					<th>Actions</th>
+				</tr>
             </thead>
         </table>
     </div>
